ae9834360a10c6d0dcd4218501089796f767c459
Oneest <oneest@macbook-pro-apple.local>
2018-08-17 12:29:40 +0300

    added ui elements

---
+++ RecorderApp/UI/RecordButton.swift
@@ -0,0 +1,29 @@
+//
+//  RecordButton.swift
+//  RecorderApp
+//
+//  Created by Oneest on 8/17/18.
+//  Copyright © 2018 Oneest. All rights reserved.
+//
+
+import UIKit
+
+class RecordButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        backgroundColor = UIColor(red: 255/255, green: 91/255, blue: 119/255, alpha: 1)
+        setTitle("", for: .normal)
+        layer.cornerRadius = self.frame.width * 0.5
+        layer.masksToBounds = true
+    }
+}
--- RecorderApp/ViewController.swift
+++ RecorderApp/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var actionButton: RecordButton!
+    @IBOutlet weak var deleteButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onActionTap(sender: UIButton) {
+        // record, play stop
     }
-
-
+    
+    @IBAction func onDeleteTap(sender: UIButton) {
+        // record, play stop
+    }
 }
 
